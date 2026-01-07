5d49f577552064f7ea5b2c801b52fa7f4fab0cfd
baptiste-marchand <baptiste-marchand@users.noreply.github.com>
2026-01-07 11:12:28 +0000

    deploy: 761e04ca490fc68530f76704324dea4ee911efe5

---
+++ rc-v0.2.0/assets/navigation.js
@@ -0,0 +1,1 @@
+window.navigationData = "data:application/octet-stream;base64,H4sIAAAAAAAAA4uuVipJrShRslLyTS1J9E0sznZMSSwoSS1S0lEqSCzJULJSSs5JLC5OLdZHU6CXUZKbo6SjlJ2Zl6JkZWhkUauDyyy/xNxUhHlliUWZiUk5mCaClKGaamxUGwsARo1z2qIAAAA="
